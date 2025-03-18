79da02744855d77da1bf3f759dbd07647558512e
MicheleFamoso <michelefamoso@gmail.com>
2025-03-18 17:06:01 +0100

    .

--- secondo/script.js
+++ secondo/script.js
@@ -1,6 +1,5 @@
 // ● Crea un contatore che tenga conto del tempo che passa, utilizzando sessionStorage. Aggiornando la pagina il valore prosegue, chiudendo la pagina - ovviamente - ricomincia. Il valore del contatore deve aggiornarsi ad ogni secondo.
 
-const timer = document.getElementById("Timer");
 const n = document.getElementById("numbers");
 
 let secondi = parseInt(sessionStorage.getItem("secondi")) || 0;
@@ -9,7 +8,6 @@ const tempo = function () {
     secondi += 1;
     console.log(secondi);
     sessionStorage.setItem("secondi", secondi);
-    let contatore = sessionStorage.getItem("secondi");
 
     n.innerText = secondi;
 
